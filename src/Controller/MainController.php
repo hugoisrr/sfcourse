bb20290dc68b137fdc56bd640a1eb7a1105d10e1
hugoisrr <84rami@gmail.com>
2021-02-06 10:34:11 +0100

    inital commit

---
+++ src/Controller/MainController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MainController extends AbstractController
+{
+    /**
+     * @Route("/", name="home")
+     */
+    public function index()
+    {
+        return new Response('<h1>Welcome Hugo</h1>');
+    }
+
+    /**
+     * @Route("/custom{name}", name="custom")
+     */
+    public function custom()
+    {
+        return new Response('<h1>Custom page<h1>');
+    }
+}
